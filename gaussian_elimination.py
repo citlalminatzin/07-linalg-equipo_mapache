eb8467c0cf39073c3c6666f761439e9b44a8465e
Paulina Alba <paulinaalba@MacBook-Pro-de-Paulina.local>
2026-04-09 15:38:15 -0600

    Se agrego funciondel ejercicio 1

--- gaussian_elimination.py
+++ gaussian_elimination.py
@@ -1,5 +1,27 @@
-def gaussian_elimination(M:list[list[float]]):
-    """Dada una matriz M, regresa una matriz triangular superior"""
+def gaussian_elimination(M: list[list[float]]):
+    n = len(M)
+    A = [fila[:] for fila in M]
+
+    for i in range(n):
+        # Pivote: Se busca el renglon o fila con el valor máximo en la columna i
+        fila_max = i
+        for k in range(i + 1, n):
+            if math.abs(A[k][i]) > math.abs(A[max_row][i]):
+                max_row = k
+        
+        # Intercambio de renglones
+        A[i], A[fila_max] = A[fila_max], A[i]
+        
+        # Eliminación 
+        if math.abs(A[i][i]) > 1e-15:
+            for j in range(i + 1, n):
+                factor = A[j][i] / A[i][i]
+                for k in range(i, n):
+                    A[j][k] -= factor * A[i][k]
+                # Pasamos el cero debajo del pivote 
+                A[j][i] = 0.0
+                
+    return A
     ...
 def triang_sup_to_diag(M:list[list[float]]):
     """
@@ -13,4 +35,4 @@ def diag(M:list[list[float]]):
 def main():
     ...
 if __name__ == "__main__":
-    main()
+    main() 
