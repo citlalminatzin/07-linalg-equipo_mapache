96f23daa4bd9988658baeba87f45ab537870d9dc
Paulina Alba <paulinaalba@MacBook-Pro-de-Paulina.local>
2026-04-10 15:02:19 -0600

    funciones ejercicio 2, paso2 y paso3

--- gaussian_elimination.py
+++ gaussian_elimination.py
@@ -1,3 +1,8 @@
+A=[[1,2,-2,1],
+[4,5,-7,6],
+[5,25,-15,-3],
+[6,-12,-6,22]]
+
 def gaussian_elimination(M: list[list[float]]):
     n = len(M)
     A = [fila[:] for fila in M]
@@ -6,14 +11,14 @@ def gaussian_elimination(M: list[list[float]]):
         # Pivote: Se busca el renglon o fila con el valor máximo en la columna i
         fila_max = i
         for k in range(i + 1, n):
-            if math.abs(A[k][i]) > math.abs(A[max_row][i]):
-                max_row = k
+            if abs(A[k][i]) > abs(A[fila_max][i]):
+                fila_max = k
         
         # Intercambio de renglones
         A[i], A[fila_max] = A[fila_max], A[i]
         
         # Eliminación 
-        if math.abs(A[i][i]) > 1e-15:
+        if abs(A[i][i]) > 1e-15:
             for j in range(i + 1, n):
                 factor = A[j][i] / A[i][i]
                 for k in range(i, n):
@@ -22,17 +27,44 @@ def gaussian_elimination(M: list[list[float]]):
                 A[j][i] = 0.0
                 
     return A
-    ...
+
+
 def triang_sup_to_diag(M:list[list[float]]):
     """
-    Dada una matriz M traingular superior 
+    Dada una matriz M triangular superior 
     regresa una matriz diagonal
     """
-    ...
+    n = len(M)
+    A = [row[:] for row in M]
+
+    for i in range(n - 1, -1, -1):
+        if abs(A[i][i]) > 1e-15:
+            for j in range(i - 1,-1,-1):
+                factor = A[j][i] / A[i][i]
+                for k in range(i,n):
+                    A[j][k] -= factor * A[i][k]
+                    A[j][i] = 0.0
+    return A
+
 def diag(M:list[list[float]]):
     """Dada una matriz regresa una matriz diagonalizada"""
-    ...
+    n = len(M)
+    Y = gaussian_elimination(M)
+    matriz_diagonal = triang_sup_to_diag(Y)
+    diagonal = [] 
+    for i in range(n): 
+        diagonal.append(matriz_diagonal[i][i]) 
+    return diagonal
+
 def main():
-    ...
+    print("Matriz original:")
+    for fila in A: print(fila)
+    
+    resultado = diag(A)
+    
+    print("\nDiagonal resultante:")
+    print(resultado)
+
 if __name__ == "__main__":
-    main() 
+    main()
+    
